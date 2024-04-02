3b982fd1f7ed4e631fa68c695c87645fd5dc4493
Mikhail Danshin <effectdopler0@gmail.com>
2024-04-02 11:05:41 +0700

    feat: add block navigation

--- entities/block/ui.tsx
+++ entities/block/ui.tsx
@@ -4,6 +4,9 @@ import { formatGwei } from 'viem'
 import { getBlockDetails, getLatestBlock } from './api'
 import { formatBaseFeePerGas, formatBurntFees, formatGasUsed } from './lib'
 import { Row } from '@/shared/ui/row'
+import { Button } from '@/shared/ui/button'
+import Link from 'next/link'
+import { ChevronLeftIcon, ChevronRightIcon } from '@radix-ui/react-icons'
 
 export const LatestBlockNumberOverview = async () => {
   const latestBlock = await getLatestBlock()
@@ -46,9 +49,23 @@ export const BlockOverview: FC<BlockOverviewProps> = async ({ number }) => {
   return (
     <Details title="block details">
       <div className="flex flex-1 flex-col gap-4">
-        <Row className="grid md:grid-cols-[minmax(0,25%),1fr] gap-x-4">
+        <Row className="grid md:grid-cols-[minmax(0,25%),1fr] gap-x-4 items-center">
           <div className="text-gray-500 md:text-black md:font-medium">Block Height</div>
-          <div>{block.number.toLocaleString()}</div>
+          <div className="flex gap-4 items-center">
+            <p>{block.number.toString()}</p>
+            <div className="flex gap-2 flex-nowrap">
+              <Button asChild={+number !== 0} variant="outline" size="icon" className="h-6 w-6" disabled={+number === 0}>
+                <Link href={`/block/${+number - 1}`}>
+                  <ChevronLeftIcon className="h-4 w-4" />
+                </Link>
+              </Button>
+              <Button asChild variant="outline" size="icon" className="h-6 w-6">
+                <Link href={`/block/${+number + 1}`}>
+                  <ChevronRightIcon className="h-4 w-4" />
+                </Link>
+              </Button>
+            </div>
+          </div>
         </Row>
 
         <Row className="grid md:grid-cols-[minmax(0,25%),1fr] gap-x-4">
