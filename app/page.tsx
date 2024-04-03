0d0a13aae0d976c5b4801610b1dfc307b8424335
Mikhail Danshin <effectdopler0@gmail.com>
2024-04-03 11:09:27 +0700

    refactor: main page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 import { LatestBlockNumberOverview, LatestBlockTransactionSizeOverview } from '@/entities/block'
 import { CurrentMarketCapOverview, CurrentPriceOverview } from '@/entities/price'
-import { SearchBar } from '@/widgets/search-bar'
 
 export const dynamic = 'force-dynamic'
 
 export default async function Home() {
   return (
-    <div className="flex flex-wrap gap-4">
+    <div className="grid grid-cols-[repeat(auto-fit,minmax(min(300px,100%),1fr))] sm:grid-cols-1 md:grid-cols-2 xl:md:grid-cols-4 gap-4">
       <LatestBlockNumberOverview />
       <LatestBlockTransactionSizeOverview />
       <CurrentPriceOverview />
