e461a6e90cf5350107a662333c05fd0d6a801a72
Mikhail Danshin <effectdopler0@gmail.com>
2024-04-03 12:09:03 +0700

    feat: init market cap chart

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
 import { LatestBlockNumberOverview, LatestBlockTransactionSizeOverview } from '@/entities/block'
 import { CurrentMarketCapOverview, CurrentPriceOverview } from '@/entities/price'
+import { Chart } from '@/widgets/chart'
 
 export const dynamic = 'force-dynamic'
 
 export default async function Home() {
   return (
-    <div className="grid grid-cols-[repeat(auto-fit,minmax(min(300px,100%),1fr))] sm:grid-cols-1 md:grid-cols-2 xl:md:grid-cols-4 gap-4">
-      <LatestBlockNumberOverview />
-      <LatestBlockTransactionSizeOverview />
-      <CurrentPriceOverview />
-      <CurrentMarketCapOverview />
-    </div>
+    <>
+      <div className="grid grid-cols-[repeat(auto-fit,minmax(min(300px,100%),1fr))] sm:grid-cols-1 md:grid-cols-2 xl:md:grid-cols-4 gap-4">
+        <LatestBlockNumberOverview />
+        <LatestBlockTransactionSizeOverview />
+        <CurrentPriceOverview />
+        <CurrentMarketCapOverview />
+      </div>
+      <Chart />
+    </>
   )
 }
