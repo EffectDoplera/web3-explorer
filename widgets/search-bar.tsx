c490dfa875b259e34fbbce2ed82d8460826f491a
Mikhail Danshin <effectdopler0@gmail.com>
2024-04-03 11:29:09 +0700

    feat: add search bar icon

--- widgets/search-bar.tsx
+++ widgets/search-bar.tsx
@@ -4,6 +4,7 @@ import { getHashType } from '@/shared/lib'
 import { Form, FormControl, FormField, FormItem } from '@/shared/ui/form'
 import { Input } from '@/shared/ui/input'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { MagnifyingGlassIcon } from '@radix-ui/react-icons'
 import { useRouter } from 'next/navigation'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
@@ -41,10 +42,11 @@ export const SearchBar = () => {
           name="block"
           render={({ field }) => (
             <FormItem>
+              <MagnifyingGlassIcon className="h-4 w-4 relative top-9 left-3" color="#cbd5e1" />
               <FormControl>
                 <Input
                   placeholder="Search by address / Txn Hash / Block"
-                  className="placeholder-shown:truncate"
+                  className="relative placeholder-shown:truncate pl-9"
                   {...field}
                 />
               </FormControl>
