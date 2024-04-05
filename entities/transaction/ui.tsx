521f3cac8a8bde22621d11e022a53de52ce511ce
Mikhail Danshin <effectdopler0@gmail.com>
2024-04-05 13:24:26 +0700

    feat: add info about transaction

--- entities/transaction/ui.tsx
+++ entities/transaction/ui.tsx
@@ -1,7 +1,10 @@
+import { cropFormatAddress } from '@/entities/address'
+import { Badge } from '@/shared/ui/badge'
 import { Details } from '@/shared/ui/details'
 import { Row } from '@/shared/ui/row'
+import Image from 'next/image'
 import { FC } from 'react'
-import { Hash } from 'viem'
+import { formatEther, formatGwei, Hash } from 'viem'
 import { getTransaction, getTransactionReceipt } from './api'
 
 interface TransactionProps {
@@ -15,51 +18,53 @@ export const Transaction: FC<TransactionProps> = async ({ hash }) => {
     <Details title="transaction details">
       <div className="flex flex-1 flex-col gap-4">
         <Row className="grid md:grid-cols-[minmax(0,25%),1fr] gap-x-4 items-center">
-          <div className="text-gray-500 md:text-black md:font-medium">Transaction Hash</div>
-          <div className="flex gap-4 items-center">
-            <p>{transaction.hash}</p>
+          <div className="text-gray-500 md:text-black md:font-medium dark:md:text-white">Transaction Hash</div>
+          <p className="break-all">{transaction.hash}</p>
+        </Row>
+
+        <Row className="grid md:grid-cols-[minmax(0,25%),1fr] gap-x-4">
+          <div className="text-gray-500 md:text-black md:font-medium dark:md:text-white">Status</div>
+          <Badge variant="outline" className="justify-self-start">
+            {transactionReceipt.status}
+          </Badge>
+        </Row>
+
+        <Row className="grid md:grid-cols-[minmax(0,25%),1fr] gap-x-4">
+          <div className="text-gray-500 md:text-black md:font-medium dark:md:text-white">Block</div>
+          <div>{transaction.blockNumber.toString()}</div>
+        </Row>
+
+        <Row className="grid md:grid-cols-[minmax(0,25%),1fr] gap-x-4">
+          <div className="text-gray-500 md:text-black md:font-medium dark:md:text-white">From</div>
+          <div>{cropFormatAddress(transaction.from)}</div>
+        </Row>
+
+        {!!transaction.to && (
+          <Row className="grid md:grid-cols-[minmax(0,25%),1fr] gap-x-4">
+            <div className="text-gray-500 md:text-black md:font-medium dark:md:text-white">To</div>
+            <div>{cropFormatAddress(transaction.to)}</div>
+          </Row>
+        )}
+
+        <Row className="grid md:grid-cols-[minmax(0,25%),1fr] gap-x-4">
+          <div className="text-gray-500 md:text-black md:font-medium dark:md:text-white">Value</div>
+          <div className="flex gap-1 items-center">
+            <Image width={20} height={20} alt="polygon  icon" src="/icons/polygon.svg" />
+            <p>{transaction.value.toString()} MATIC</p>
           </div>
         </Row>
 
         <Row className="grid md:grid-cols-[minmax(0,25%),1fr] gap-x-4">
-          <div className="text-gray-500 md:text-black md:font-medium">Status</div>
-          <div>{transactionReceipt.status}</div>
+          <div className="text-gray-500 md:text-black md:font-medium dark:md:text-white">Transaction Fees</div>
+          <div>0.013013802095944151 MATIC ($0.01) ????</div>
         </Row>
 
-        <Row className="grid md:grid-cols-[minmax(0,25%),1fr] gap-x-4">
-          <div className="text-gray-500 md:text-black md:font-medium">Block</div>
-        </Row>
-
-        <Row className="grid md:grid-cols-[minmax(0,25%),1fr] gap-x-4">
-          <div className="text-gray-500 md:text-black md:font-medium">Timestamp</div>
-        </Row>
-
-        <Row className="grid md:grid-cols-[minmax(0,25%),1fr] gap-x-4">
-          <div className="text-gray-500 md:text-black md:font-medium">From</div>
-        </Row>
-
-        <Row className="grid md:grid-cols-[minmax(0,25%),1fr] gap-x-4">
-          <div className="text-gray-500 md:text-black md:font-medium">To</div>
-        </Row>
-
-        {!!transaction.gas && (
+        {!!transaction.gasPrice && (
           <Row className="grid md:grid-cols-[minmax(0,25%),1fr] gap-x-4">
-            <div className="text-gray-500 md:text-black md:font-medium">Value</div>
-            <div>0</div>
-          </Row>
-        )}
-
-        {!!transaction.gas && (
-          <Row className="grid md:grid-cols-[minmax(0,25%),1fr] gap-x-4">
-            <div className="text-gray-500 md:text-black md:font-medium">Transaction Fees</div>
-            <div>0</div>
-          </Row>
-        )}
-
-        {!!transaction.gas && (
-          <Row className="grid md:grid-cols-[minmax(0,25%),1fr] gap-x-4">
-            <div className="text-gray-500 md:text-black md:font-medium">Gas Price</div>
-            <div>0</div>
+            <div className="text-gray-500 md:text-black md:font-medium dark:md:text-white">Gas Price</div>
+            <div>
+              {formatGwei(transaction.gasPrice)} Gwei ({formatEther(transaction.gasPrice)}) MATIC
+            </div>
           </Row>
         )}
       </div>
