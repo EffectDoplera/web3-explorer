9cef36e803fc9dc087aa01560a7b6912c87dae15
Mikhail Danshin <effectdopler0@gmail.com>
2024-04-11 15:09:50 +0300

    refactor: details

--- shared/ui/details.tsx
+++ shared/ui/details.tsx
@@ -1,6 +1,7 @@
-import Image from 'next/image'
+import { cva, VariantProps } from 'class-variance-authority'
 import Link from 'next/link'
-import { FC, PropsWithChildren, ReactNode } from 'react'
+import React, { FC, PropsWithChildren, ReactNode } from 'react'
+import { cn } from '../lib'
 import { Card, CardContent, CardHeader, CardTitle } from './card'
 
 interface DetailsProps {
@@ -18,7 +19,7 @@ export const Details: FC<PropsWithChildren<DetailsProps>> = ({ children, title, 
       {image}
       <CardTitle className="leading-normal first-letter:uppercase truncate">{title}</CardTitle>
     </CardHeader>
-    <CardContent className="flex p-3 items-center justify-between bg-white-cream-100 font-semibold">
+    <CardContent className="flex flex-1 flex-col gap-4 p-3 bg-white-cream-100 font-semibold">
       {children}
       {link && (
         <Link className="text-primary font-semibold text-sm hover:underline focus:underline truncate" href={link.href}>
@@ -28,3 +29,41 @@ export const Details: FC<PropsWithChildren<DetailsProps>> = ({ children, title, 
     </CardContent>
   </Card>
 )
+
+const rowVariants = cva('text-md font-medium', {
+  variants: {
+    variant: {
+      default: 'grid md:grid-cols-[minmax(0,25%),1fr] gap-x-4 items-center',
+    },
+  },
+  defaultVariants: {
+    variant: 'default',
+  },
+})
+
+export const DetailsRow = React.forwardRef<
+  React.ElementRef<'div'>,
+  React.ComponentPropsWithoutRef<'div'> & VariantProps<typeof rowVariants>
+>(({ className, variant, ...props }, ref) => (
+  <div ref={ref} className={cn(rowVariants({ variant, className }))} {...props} />
+))
+
+DetailsRow.displayName = 'DetailsRow'
+
+export const DetailsRowLabel = React.forwardRef<React.ElementRef<'div'>, React.ComponentPropsWithoutRef<'div'>>(
+  ({ className, ...props }, ref) => (
+    <div
+      ref={ref}
+      className={cn('text-gray-500 md:text-black md:font-medium dark:md:text-white', className)}
+      {...props}
+    />
+  ),
+)
+
+DetailsRowLabel.displayName = 'DetailsRowLabel'
+
+export const DetailsRowInfo = React.forwardRef<React.ElementRef<'div'>, React.ComponentPropsWithoutRef<'div'>>(
+  ({ className, ...props }, ref) => <div ref={ref} className={cn(className)} {...props} />,
+)
+
+DetailsRowInfo.displayName = 'DetailsRowInfo'
