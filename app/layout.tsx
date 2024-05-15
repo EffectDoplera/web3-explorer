8e152e78f0e7aaffddd07a91948e27abbe8601e1
Mikhail Danshin <effectdopler0@gmail.com>
2024-05-15 17:18:57 +0300

    feat: add speed-insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import { cn } from '@/shared/lib'
 import { SearchBar } from '@/widgets/search-bar'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import type { Metadata, Viewport } from 'next'
 import { Inter } from 'next/font/google'
 import { Footer } from './footer'
@@ -44,6 +45,7 @@ export default function RootLayout({
             </div>
           </div>
         </ThemeProvider>
+        <SpeedInsights />
       </body>
     </html>
   )
