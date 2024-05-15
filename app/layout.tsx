8e86cf801345b7fc5b1087adf328dbe0f4dc81b7
Mikhail Danshin <effectdopler0@gmail.com>
2024-05-15 17:24:53 +0300

    feat: add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import { cn } from '@/shared/lib'
 import { SearchBar } from '@/widgets/search-bar'
+import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import type { Metadata, Viewport } from 'next'
 import { Inter } from 'next/font/google'
@@ -46,6 +47,7 @@ export default function RootLayout({
           </div>
         </ThemeProvider>
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   )
