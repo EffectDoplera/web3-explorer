31d7438859e2351576df0cba27d095ea8d4c6e31
Mikhail Danshin <effectdopler0@gmail.com>
2024-05-16 10:17:28 +0300

    fix: text colors

--- app/address/[hash]/page.tsx
+++ app/address/[hash]/page.tsx
@@ -16,7 +16,7 @@ export default async function AddressPage({ params }: AddressPageProps) {
     <div className="flex flex-1 justify-center flex-col gap-4">
       <div className="flex flex-wrap items-center gap-2">
         <h3 className="text-xl font-medium">Address</h3>
-        <div className="text-gray-500">
+        <div>
           <div className="flex items-center gap-1">
             <Avatar>
               {/* TODO: Add Image */}
--- entities/address/ui.tsx
+++ entities/address/ui.tsx
@@ -1,4 +1,4 @@
-import { Details } from '@/shared/ui/details'
+import { Details, DetailsRow, DetailsRowInfo, DetailsRowLabel } from '@/shared/ui/details'
 import { Row } from '@/shared/ui/row'
 import { BackpackIcon } from '@radix-ui/react-icons'
 import Image from 'next/image'
@@ -15,31 +15,26 @@ export const Address: FC<AddressProps> = async ({ hash }) => {
 
   return (
     <Details image={<BackpackIcon width={20} height={20} />} title="overview">
-      <div className="flex flex-1 flex-col gap-4">
-        <Row className="grid md:grid-cols-[minmax(0,25%),1fr] gap-x-4">
-          <div className="text-gray-500 md:text-black md:font-medium">Matic Balance</div>
-          <div className="flex items-center gap-1">
-            <Image width={20} height={20} alt="polygon token icon" src="/icons/polygon.svg" />
-            <p>
-              {new Intl.NumberFormat('en', {
-                minimumFractionDigits: 10,
-              }).format(+formatEther(balance))}
-            </p>
-            <p className="uppercase">matic</p>
-          </div>
-        </Row>
+      <DetailsRow>
+        <DetailsRowLabel>Matic Balance</DetailsRowLabel>
+        <DetailsRowInfo className="flex items-center gap-1">
+          <Image width={20} height={20} alt="polygon token icon" src="/icons/polygon.svg" />
+          {new Intl.NumberFormat('en', {
+            minimumFractionDigits: 10,
+          }).format(+formatEther(balance))}
+        </DetailsRowInfo>
+      </DetailsRow>
 
-        <Row className="grid md:grid-cols-[minmax(0,25%),1fr] gap-x-4">
-          <div className="text-gray-500 md:text-black md:font-medium">Matic Value</div>
-          <p>
-            {new Intl.NumberFormat('en', {
-              style: 'currency',
-              currency: 'USD',
-              minimumFractionDigits: 2,
-            }).format(usdBalance)}
-          </p>
-        </Row>
-      </div>
+      <DetailsRow>
+        <DetailsRowLabel>Matic Value</DetailsRowLabel>
+        <DetailsRowInfo>
+          {new Intl.NumberFormat('en', {
+            style: 'currency',
+            currency: 'USD',
+            minimumFractionDigits: 2,
+          }).format(usdBalance)}
+        </DetailsRowInfo>
+      </DetailsRow>
     </Details>
   )
 }
