97fccbd8e78db550f45f269f2634dbb40481d56e
Mikhail Danshin <effectdopler0@gmail.com>
2024-05-17 13:18:24 +0300

    feat(test): init pwa

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,9 +18,49 @@ export const viewport: Viewport = {
   ],
 }
 
+const APP_NAME = 'Polygon explorer'
+const APP_DEFAULT_TITLE = 'Polygon explorer'
+const APP_TITLE_TEMPLATE = '%s - PWA App'
+const APP_DESCRIPTION = 'Explore matic transactions'
+
 export const metadata: Metadata = {
-  title: 'Polygon explorer',
-  description: 'Explore matic transactions',
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: 'manifest.json',
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'default',
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: 'website',
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: 'summary',
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  icons: [
+    { rel: 'icon', url: '/icons/favicon.ico' },
+    { rel: 'apple-touch-icon', url: '/icons/apple-touch-icon.png' },
+  ],
 }
 
 export default function RootLayout({
@@ -30,9 +70,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-      </head>
       <body className={cn('min-h-screen bg-background font-sans antialiased', inter.className)}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           <div className="min-h-screen max-h-screen flex">
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
+import withPWA from '@ducanh2912/next-pwa'
 import withBundleAnalyzer from '@next/bundle-analyzer'
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+}
 
-export default process.env.ANALYZE === 'true' ? withBundleAnalyzer(nextConfig) : nextConfig
+export default process.env.ANALYZE === 'true'
+  ? withBundleAnalyzer(nextConfig)
+  : withPWA({
+      dest: 'public',
+    })(nextConfig)
